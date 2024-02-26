c8449d8d7111af0aac5c0d43a4f23429349c20f0
Trevor <toantu3005@gmail.com>
2024-02-26 23:02:21 +1100

    Polymorphism completed - from surface pro

--- src/Exercise.java
+++ src/Exercise.java
@@ -5,37 +5,3 @@ public class Exercise {
     }
 
 }
-
-class Printer {
-    private int tonerLevel;
-    private int pagesPrinted;
-    private boolean duplex;
-
-    public Printer(int tonerLevel, boolean duplex) {
-        this.tonerLevel = (tonerLevel >= 0 && tonerLevel <= 100) ? tonerLevel : 1;
-        this.duplex = duplex;
-        this.pagesPrinted = 0;
-    }
-
-    public int addToner(int tonerAmount) {
-        if (tonerAmount > 0 && tonerAmount <= 100) {
-            int tempAmount = tonerLevel + tonerAmount;
-            if (tempAmount > 100) {
-                return -1;
-            }
-            tonerLevel += tonerAmount;
-            return tonerLevel;
-        }
-        return -1;
-    }
-
-    public int printPages(int pages) {
-        int pagesToPrint= (duplex) ? (pages / 2) + (pages % 2) : pages;
-        pagesPrinted += pagesToPrint;
-        return pagesToPrint;
-    }
-
-    public int getPagesPrinted() {
-        return pagesPrinted;
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
+        Car car = new Car("Ferrari GTS");
+        runRace(car);
 
-        car.setMake("Porsche");
-        car.setModel("Carrera");
-        car.setDoors(2);
-        car.setConvertible(true);
-        car.setColor("Black");
-        System.out.println("make = " + car.getMake());
-        System.out.println("model = " + car.getModel());
-        car.describeCar();
+        Car ferrari = new GasPoweredCar("Ferrari GTS",
+                15.4, 6);
+        runRace(ferrari);
 
-        Car targa = new Car();
-        targa.setMake("Porsche");
-        targa.setModel("Targa");
-        targa.setDoors(2);
-        targa.setConvertible(false);
-        targa.setColor("Red");
-        targa.describeCar();
+        Car tesla = new ElectricCar("Tesla Model 3",
+                568, 75);
+        runRace(tesla);
+
+        Car ferrariHybrid = new HybridCar("Ferrari SF90",
+                16, 8, 8);
+        runRace(ferrariHybrid);
+    }
+
+    public static void runRace(Car car) {
+        car.startEngine();
+        car.drive();
     }
 
 }
