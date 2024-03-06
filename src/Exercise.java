656e9d5b84dfbc3a15d3dde82d329ddeb8644690
Trevor <toantu3005@gmail.com>
2024-03-07 01:14:49 +1100

    Generics - from Desktop

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise {
 
     public static void main(String[] args) {
@@ -6,271 +9,130 @@ public class Exercise {
 
 }
 
-abstract class ListItem {
-    protected ListItem rightLink = null;
-    protected ListItem leftLink = null;
-    protected Object value;
-
-    public ListItem(Object value) {
-        this.value = value;
-    }
-
-    abstract ListItem next();
-    abstract ListItem setNext(ListItem item);
-    abstract ListItem previous();
-    abstract ListItem setPrevious(ListItem item);
-    abstract int compareTo(ListItem item);
-
-    public Object getValue() {
-        return value;
-    }
-
-    public void setValue(Object value) {
-        this.value = value;
-    }
+interface ISaveable {
+    List<String> write();
+    void read(List<String> strings);
 }
 
-class Node extends ListItem {
+class Player implements ISaveable {
+    private String name;
+    private String weapon = "Sword";
+    private int hitPoints;
+    private int strength;
 
-    public Node(Object value) {
-        super(value);
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
     }
 
     @Override
-    ListItem next() {
-        return rightLink;
+    public List<String> write() {
+        List<String> info = new ArrayList<>();
+        info.add(getName());
+        info.add(String.valueOf(getHitPoints()));
+        info.add(String.valueOf(getStrength()));
+        info.add(getWeapon());
+        return info;
     }
 
     @Override
-    ListItem setNext(ListItem item) {
-        rightLink = item;
-        return rightLink;
-    }
-
-    @Override
-    ListItem previous() {
-        return leftLink;
-    }
-
-    @Override
-    ListItem setPrevious(ListItem item) {
-        leftLink = item;
-        return leftLink;
-    }
-
-    @Override
-    int compareTo(ListItem item) {
-        if (item != null) {
-            return ((String) super.getValue()).compareTo((String) item.getValue());
-        } else {
-            return -1;
-        }
-    }
-
-}
-
-class MyLinkedList implements NodeList {
-    private ListItem root;
-
-    public MyLinkedList(ListItem root) {
-        this.root = root;
-    }
-
-    @Override
-    public ListItem getRoot() {
-        return root;
-    }
-
-    @Override
-    public boolean addItem(ListItem item) {
-        if (root == null) {
-            root = item;
-            return true;
-        }
-
-        ListItem currentItem = root;
-
-        while (currentItem != null) {
-            int index = currentItem.compareTo(item);
-            if (index < 0) {
-                if (currentItem.next() != null) {
-                    currentItem = currentItem.next();
-                } else {
-                    currentItem.setNext(item).setPrevious(currentItem);
-                    return true;
-                }
-            } else if (index > 0) {
-                if (currentItem.previous() != null) {
-                    currentItem.previous().setNext(item).setPrevious(currentItem.previous());
-                    item.setNext(currentItem).setPrevious(item);
-                } else {
-                    item.setNext(this.root).setPrevious(item);
-                    this.root = item;
-                }
-                return true;
-            } else {
-                return false;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public boolean removeItem(ListItem item) {
-        if (item != null) {
-            System.out.println("Deleting item " + item.getValue());
-        }
-
-        ListItem currentItem = this.root;
-        while (currentItem != null) {
-            int index = currentItem.compareTo(item);
-            if (index == 0) {
-                if (currentItem == this.root) {
-                    this.root = currentItem.next();
-                } else {
-                    currentItem.previous().setNext(currentItem.next());
-                    if (currentItem.next() != null) {
-                        currentItem.next().setPrevious(currentItem.previous());
-                    }
-                }
-                return true;
-            } else if (index < 0) {
-                currentItem = currentItem.next();
-            } else {
-                return false;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public void traverse(ListItem root) {
-        if (root == null) {
-            System.out.println("The list is empty");
+    public void read(List<String> strings) {
+        if (strings == null || strings.isEmpty()) {
             return;
         }
-        while  (root != null) {
-            System.out.println(root.getValue());
-            root = root.next();
-        }
+        setName(strings.get(0));
+        setHitPoints(Integer.parseInt(strings.get(1)));
+        setStrength(Integer.parseInt(strings.get(2)));
+        setWeapon(strings.get(3));
+    }
+
+    @Override
+    public String toString() {
+        return """
+                Player{name='%s', hitPoints=%d, strength=%d, weapon='%s'}%n"""
+                .formatted(getName(), getHitPoints(), getStrength(), getWeapon());
     }
 }
 
-class SearchTree implements NodeList {
-    private ListItem root = null;
+class Monster implements ISaveable {
+    private String name;
+    private int hitPoints;
+    private int strength;
 
-    public SearchTree(ListItem root) {
-        this.root = root;
+    public Monster(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
     }
 
     @Override
-    public ListItem getRoot() {
-        return this.root;
+    public List<String> write() {
+        List<String> info = new ArrayList<>();
+        info.add(getName());
+        info.add(String.valueOf(getHitPoints()));
+        info.add(String.valueOf(getStrength()));
+        return info;
     }
 
     @Override
-    public boolean addItem(ListItem item) {
-        if (this.root == null) {
-            this.root = item;
-            return true;
+    public void read(List<String> strings) {
+        if (strings == null || strings.isEmpty()) {
+            return;
         }
-
-        ListItem currentItem = this.root;
-        while (currentItem != null) {
-            int index = currentItem.compareTo(item);
-            if (index < 0) {
-                if (currentItem.next() != null) {
-                    currentItem = currentItem.next();
-                } else {
-                    currentItem.setNext(item);
-                    return true;
-                }
-            } else if (index > 0) {
-                if (currentItem.previous() != null) {
-                    currentItem = currentItem.previous();
-                } else {
-                    currentItem.setPrevious(item);
-                    return true;
-                }
-            } else {
-                System.out.println(item.getValue() + " is already present");
-                return false;
-            }
-        }
-        return false;
+        name = strings.get(0);
+        hitPoints = Integer.parseInt(strings.get(1));
+        strength = Integer.parseInt(strings.get(2));
     }
 
     @Override
-    public boolean removeItem(ListItem item) {
-        if (item != null) {
-            System.out.println("Deleting item " + item.getValue());
-        }
-
-        ListItem currentItem = this.root;
-        ListItem parentItem = currentItem;
-
-        while (currentItem != null) {
-            int index = currentItem.compareTo(item);
-            if (index < 0) {
-                parentItem = currentItem;
-                currentItem = currentItem.next();
-            } else if (index > 0) {
-                parentItem = currentItem;
-                currentItem = currentItem.previous();
-            } else {
-                performRemoval(currentItem, parentItem);
-                return true;
-            }
-        }
-        return false;
+    public String toString() {
+        return """
+                Monster{name='%s', hitPoints=%d, strength=%d}%n"""
+                .formatted(getName(), getHitPoints(), getStrength());
     }
-
-    private void performRemoval(ListItem item, ListItem parent) {
-        if (item.next() == null) {
-            if (parent.next() == item) {
-                parent.setNext(item.previous());
-            } else if (parent.previous() == item) {
-                parent.setPrevious(item.previous());
-            } else {
-                this.root = item.previous();
-            }
-        } else if (item.previous() == null) {
-            if (parent.next() == item) {
-                parent.setNext(item.next());
-            } else if (parent.previous() == item) {
-                parent.setPrevious(item.next());
-            } else {
-                this.root = item.next();
-            }
-        } else {
-            ListItem current = item.next();
-            ListItem leftmostParent = item;
-            while (current.previous() != null) {
-                leftmostParent = current;
-                current = current.previous();
-            }
-            item.setValue(current.getValue());
-            if (leftmostParent == item) {
-                item.setNext(current.next());
-            } else {
-                leftmostParent.setPrevious(current.next());
-            }
-        }
-    }
-
-    @Override
-    public void traverse(ListItem root) {
-        if (root != null) {
-            traverse(root.previous());
-            System.out.println(root.getValue());
-            traverse(root.next());
-        }
-    }
-}
-
-interface NodeList {
-    ListItem getRoot();
-    boolean addItem(ListItem item);
-    boolean removeItem(ListItem item);
-    void traverse(ListItem root);
 }
